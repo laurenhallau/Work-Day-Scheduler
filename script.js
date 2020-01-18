8c3c799acfa5820e07f723389311d55832419b15
Lauren Hallau <lhallau@gmail.com>
2020-01-18 17:22:04 -0600

    buttons saving to console log

---
+++ script.js
@@ -0,0 +1,43 @@
+//This will be the moment.js function
+$(document).ready(function () {
+
+}) 
+
+//Saving information when save button is clicked- One for each hour. text id 
+// Save buttons for each hour.  The origin textarea id is passed with the appointment details
+$("#saveButtonNineAM").click(function () {
+    console.log("User clicked save at 9am");
+})
+
+$("#saveButtonTenAM").click(function () {
+    console.log("User clicked save at 10am");
+})
+
+$("#saveButtonElevenAM").click(function () {
+    console.log("User clicked save at 11am");
+})
+
+$("#saveButtonTwelvePM").click(function () {
+    console.log("User clicked save at 12pm");
+})
+
+$("#saveButtonOnePM").click(function () {
+    console.log("User clicked save at 1pm");
+})
+
+$("#saveButtonTwoPM").click(function () {
+    console.log("User clicked save at 2pm");
+})
+
+$("#saveButtonThreePM").click(function () {
+    console.log("User clicked save at 3pm");
+})
+
+$("#saveButtonFourPM").click(function () {
+    console.log("User clicked save at 4pm");
+})
+
+$("#saveButtonFivePM").click(function () {
+    console.log("User clicked save at 5pm");
+})
+
