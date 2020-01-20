18e6ff0c10d278a8ad015b948dd97a7959df058b
Lauren Hallau <lhallau@gmail.com>
2020-01-20 17:28:12 -0600

    coding after tutoring session with Joey. added local storage and color to moment

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 $(document).ready(function () {
     console.log(moment()); //testing
     $("#currentDay").text(moment().format('dddd') + ", " + moment().format('LLL'));
-}) 
+
 
 //Saving information when save button is clicked- One for each hour. text id 
 // Save buttons for each hour.  The origin textarea id is passed with the appointment details
 $("#saveButtonNineAM").click(function () {
     console.log("User clicked save at 9am");
+    var value = $("#textAreaNineAM").val();
+    var time = "NineAM";
+    console.log(value);
+    localStorage.setItem(time, value);
 })
 
 $("#saveButtonTenAM").click(function () {
@@ -42,3 +46,30 @@ $("#saveButtonFivePM").click(function () {
     console.log("User clicked save at 5pm");
 })
 
+function hourChecker() {
+    var currentHour = moment().hours();
+    console.log(currentHour);
+    //running a function for each class named 'row'
+    $(".row").each(function() {
+    var blockHour = parseInt($(this).attr("id"));
+    if (blockHour < currentHour) {
+        console.log("past");
+        $(this).addClass("past");
+    }
+    else if (blockHour === currentHour) {
+        console.log("present");
+        $(this).addClass("present");
+    }
+    else {
+        console.log("future");
+        $(this).addClass("future");
+    }
+    
+    })
+}
+hourChecker();
+
+console.log(localStorage.getItem("NineAM"));
+$("#textAreaNineAM").val(localStorage.getItem("NineAM"));
+
+})
